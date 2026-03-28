e9e5d32c029f1abddfb5599ca809c4a80242fadd
touhidss <135936344+touhidss@users.noreply.github.com>
2026-03-28 11:47:03 +0530

    Create catalog.ts
    new hai

---
+++ providers/hianime/catalog.ts
@@ -0,0 +1,20 @@
+export const catalog = [
+  {
+    title: "Recent",
+    filter: "/recently-updated",
+  },
+  {
+    title: "Top Airing",
+    filter: "/top-airing",
+  },
+  {
+    title: "Most Popular",
+    filter: "/most-popular",
+  },
+  {
+    title: "Movies",
+    filter: "/movie",
+  },
+];
+
+export const genres = [];

0848f8e2c9cbee580af4b1f78faed81c26bafa38
touhidss <135936344+touhidss@users.noreply.github.com>
2026-03-28 16:47:46 +0530

    Update posts.ts

--- providers/hanime/posts.ts
+++ providers/hanime/posts.ts
@@ -1,4 +1,15 @@
 import { Post } from "../types";
 
-export const getPosts = async (): Promise<Post[]> => [];
-export const getSearchPosts = async (): Promise<Post[]> => [];
+export const getPosts = async (): Promise<Post[]> => {
+  return [
+    {
+      title: "Open Hanime",
+      link: "welcome-to-the-hentai-world",
+      image: "https://placehold.co/300x400",
+    },
+  ];
+};
+
+export const getSearchPosts = async (): Promise<Post[]> => {
+  return [];
+};

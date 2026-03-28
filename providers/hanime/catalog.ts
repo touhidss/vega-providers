84e8126cd6227828a0ba1e3a4d364bf1e7e0e530
touhidss <135936344+touhidss@users.noreply.github.com>
2026-03-28 16:28:38 +0530

    Update catalog.ts

--- providers/hanime/catalog.ts
+++ providers/hanime/catalog.ts
@@ -1,8 +1,8 @@
 export const catalog = [
   { title: "Trending", filter: "trending" },
-  { title: "Recent Uploads", filter: "recent_uploads" },
-  { title: "New Releases", filter: "new_releases" },
-  { title: "Recent Images", filter: "recent_images" },
+  { title: "Latest", filter: "latest" },
+  { title: "Popular", filter: "popular" },
+  { title: "Most Viewed", filter: "views" },
   { title: "Random", filter: "random" },
 ];
 

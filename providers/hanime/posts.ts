2b54a640d6b7a54bcc2c9d211ea843674d302265
touhidss <135936344+touhidss@users.noreply.github.com>
2026-03-28 16:24:44 +0530

    Update posts.ts

--- providers/hanime/posts.ts
+++ providers/hanime/posts.ts
@@ -4,35 +4,34 @@ export const getPosts = async function ({
   filter,
   signal,
   providerContext,
-}: {
-  filter: string;
-  page: number;
-  providerValue: string;
-  signal: AbortSignal;
-  providerContext: ProviderContext;
-}): Promise<Post[]> {
+}: any): Promise<Post[]> {
   try {
     const { axios } = providerContext;
 
     const res = await axios.get(
-      `https://members.hanime.tv/api/v8/browse?order=${filter}`,
+      `https://hanime.tv/videos/hentai?order=${filter}`,
       { signal }
     );
 
-    const data = res.data?.hentai_videos || [];
-    const catalog: Post[] = [];
+    const html = res.data;
 
-    data.forEach((e: any) => {
-      if (e?.name && e?.slug && e?.cover_url) {
-        catalog.push({
-          title: e.name,
-          link: e.slug,
-          image: e.cover_url,
-        });
-      }
+    const matches = [
+      ...html.matchAll(
+        /<a class="overlay" href="\/videos\/hentai\/(.*?)".*?<img src="(.*?)".*?alt="(.*?)"/gs
+      ),
+    ];
+
+    const posts: Post[] = [];
+
+    matches.forEach((m: any) => {
+      posts.push({
+        title: m[3],
+        link: m[1],
+        image: m[2],
+      });
     });
 
-    return catalog;
+    return posts;
   } catch {
     return [];
   }
@@ -42,35 +41,34 @@ export const getSearchPosts = async function ({
   searchQuery,
   signal,
   providerContext,
-}: {
-  searchQuery: string;
-  page: number;
-  providerValue: string;
-  signal: AbortSignal;
-  providerContext: ProviderContext;
-}): Promise<Post[]> {
+}: any): Promise<Post[]> {
   try {
     const { axios } = providerContext;
 
     const res = await axios.get(
-      `https://members.hanime.tv/api/v8/search?q=${encodeURIComponent(searchQuery)}`,
+      `https://hanime.tv/search?query=${searchQuery}`,
       { signal }
     );
 
-    const data = res.data?.hentai_videos || [];
-    const catalog: Post[] = [];
+    const html = res.data;
 
-    data.forEach((e: any) => {
-      if (e?.name && e?.slug && e?.cover_url) {
-        catalog.push({
-          title: e.name,
-          link: e.slug,
-          image: e.cover_url,
-        });
-      }
+    const matches = [
+      ...html.matchAll(
+        /<a class="overlay" href="\/videos\/hentai\/(.*?)".*?<img src="(.*?)".*?alt="(.*?)"/gs
+      ),
+    ];
+
+    const posts: Post[] = [];
+
+    matches.forEach((m: any) => {
+      posts.push({
+        title: m[3],
+        link: m[1],
+        image: m[2],
+      });
     });
 
-    return catalog;
+    return posts;
   } catch {
     return [];
   }

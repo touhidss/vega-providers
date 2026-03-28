ee6d647d1f3aee7cd606782f2b59aa10931e658c
touhidss <135936344+touhidss@users.noreply.github.com>
2026-03-28 15:26:37 +0530

    Update meta.ts

--- providers/hanime/meta.ts
+++ providers/hanime/meta.ts
@@ -8,34 +8,28 @@ export const getMeta = async function ({
   providerContext: ProviderContext;
 }): Promise<Info> {
   try {
-    const { axios, getBaseUrl } = providerContext;
-    const baseUrl = await getBaseUrl("consumet");
-    const url = `${baseUrl}/movies/flixhq/info?id=` + id;
-    const res = await axios.get(url);
+    const { axios } = providerContext;
+
+    const res = await axios.get(`https://hanime.tv/api/v8/video?id=${id}`);
     const data = res.data;
+
     const meta = {
-      title: data.title,
-      synopsis: data.description.replace(/<[^>]*>?/gm, "").trim(),
-      image: data.cover,
-      cast: data.casts,
-      rating: data.rating,
-      tags: [data?.type, data?.duration, data.releaseDate.split("-")[0]],
+      title: data.name,
+      synopsis: data.description,
+      image: data.cover_url,
       imdbId: "",
-      type: data.episodes.length > 1 ? "series" : "movie",
+      type: "movie",
     };
 
     const links: Link["directLinks"] = [];
-    data.episodes.forEach((episode: any) => {
-      const title = episode?.number
-        ? "Season-" + episode?.season + " Ep-" + episode.number
-        : episode.title;
-      const link = episode.id + "*" + data.id;
-      if (link && title) {
+
+    data.videos_manifest.servers.forEach((server: any) => {
+      server.streams.forEach((stream: any) => {
         links.push({
-          title,
-          link,
+          title: `${stream.height}p`,
+          link: stream.url,
         });
-      }
+      });
     });
 
     return {
@@ -48,7 +42,6 @@ export const getMeta = async function ({
       ],
     };
   } catch (err) {
-    console.error(err);
     return {
       title: "",
       synopsis: "",

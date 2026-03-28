0d96f3fb4f6648017c1317b91d46bde1c6e6abbf
touhidss <135936344+touhidss@users.noreply.github.com>
2026-03-28 15:48:04 +0530

    Update posts.js

--- dist/hanime/posts.js
+++ dist/hanime/posts.js
@@ -2,44 +2,33 @@
 Object.defineProperty(exports,"__esModule",{value:true});
 exports.getPosts=exports.getSearchPosts=void 0;
 
-exports.getPosts=async({signal,providerContext})=>{
+exports.getPosts=async({filter,signal,providerContext})=>{
 try{
 const{axios}=providerContext;
-const res=await axios.get("https://hanime.tv/api/v8/browse",{signal});
-const data=res.data?.results||res.data;
-const catalog=[];
-data?.forEach(e=>{
+const order=filter==="/recent-movies"?"latest":filter==="/recent-shows"?"popular":"trending";
+const res=await axios.get(`https://hanime.tv/api/v8/browse?page=0&order=${order}`,{signal});
+const data=res.data?.hentai_videos||[];
+const out=[];
+data.forEach(e=>{
 if(e?.name&&e?.slug&&e?.cover_url){
-catalog.push({
-title:e.name,
-link:e.slug,
-image:e.cover_url
-});
+out.push({title:e.name,link:e.slug,image:e.cover_url});
 }
 });
-return catalog;
-}catch{
-return [];
-}
+return out;
+}catch{return[];}
 };
 
 exports.getSearchPosts=async({searchQuery,signal,providerContext})=>{
 try{
 const{axios}=providerContext;
-const res=await axios.get(`https://hanime.tv/api/v8/search?q=${searchQuery}`,{signal});
-const data=res.data?.results||res.data;
-const catalog=[];
-data?.forEach(e=>{
+const res=await axios.get(`https://hanime.tv/api/v8/search?q=${encodeURIComponent(searchQuery)}`,{signal});
+const data=res.data?.hentai_videos||[];
+const out=[];
+data.forEach(e=>{
 if(e?.name&&e?.slug&&e?.cover_url){
-catalog.push({
-title:e.name,
-link:e.slug,
-image:e.cover_url
-});
+out.push({title:e.name,link:e.slug,image:e.cover_url});
 }
 });
-return catalog;
-}catch{
-return [];
-}
+return out;
+}catch{return[];}
 };

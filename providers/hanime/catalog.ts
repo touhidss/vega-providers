9533470b82e9b3ac959b73c59a9b3ed1dc7fef13
touhidss <135936344+touhidss@users.noreply.github.com>
2026-03-28 15:16:35 +0530

    Update catalog.ts

--- providers/hanime/catalog.ts
+++ providers/hanime/catalog.ts
@@ -1,1 +1,16 @@
-jjh
+export const catalog = [
+  {
+    title: "Trending",
+    filter: "/trending",
+  },
+  {
+    title: "Movies",
+    filter: "/recent-movies",
+  },
+  {
+    title: "TV Shows",
+    filter: "/recent-shows",
+  },
+];
+
+export const genres = [];

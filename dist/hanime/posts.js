07187ab0fcf13af8bd5d77c25fbefeeff6d89aaa
touhidss <135936344+touhidss@users.noreply.github.com>
2026-03-28 16:49:09 +0530

    Update posts.js

--- dist/hanime/posts.js
+++ dist/hanime/posts.js
@@ -1,75 +1,13 @@
-import { Post, ProviderContext } from "../types";
+"use strict";
+Object.defineProperty(exports,"__esModule",{value:true});
+exports.getPosts=exports.getSearchPosts=void 0;
 
-export const getPosts = async function ({
-  filter,
-  signal,
-  providerContext,
-}: any): Promise<Post[]> {
-  try {
-    const { axios } = providerContext;
-
-    const res = await axios.get(
-      `https://hanime.tv/videos/hentai?order=${filter}`,
-      { signal }
-    );
-
-    const html = res.data;
-
-    const matches = [
-      ...html.matchAll(
-        /<a class="overlay" href="\/videos\/hentai\/(.*?)".*?<img src="(.*?)".*?alt="(.*?)"/gs
-      ),
-    ];
-
-    const posts: Post[] = [];
-
-    matches.forEach((m: any) => {
-      posts.push({
-        title: m[3],
-        link: m[1],
-        image: m[2],
-      });
-    });
-
-    return posts;
-  } catch {
-    return [];
-  }
+exports.getPosts=async()=>{
+return [{
+title:"Open Hanime",
+link:"welcome-to-the-hentai-world",
+image:"https://placehold.co/300x400"
+}];
 };
 
-export const getSearchPosts = async function ({
-  searchQuery,
-  signal,
-  providerContext,
-}: any): Promise<Post[]> {
-  try {
-    const { axios } = providerContext;
-
-    const res = await axios.get(
-      `https://hanime.tv/search?query=${searchQuery}`,
-      { signal }
-    );
-
-    const html = res.data;
-
-    const matches = [
-      ...html.matchAll(
-        /<a class="overlay" href="\/videos\/hentai\/(.*?)".*?<img src="(.*?)".*?alt="(.*?)"/gs
-      ),
-    ];
-
-    const posts: Post[] = [];
-
-    matches.forEach((m: any) => {
-      posts.push({
-        title: m[3],
-        link: m[1],
-        image: m[2],
-      });
-    });
-
-    return posts;
-  } catch {
-    return [];
-  }
-};
+exports.getSearchPosts=async()=>[];
